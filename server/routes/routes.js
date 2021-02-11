fb19f6246304bb6228879bf3068ea4eecda52c41
sweta222 <swetastuti2@gmail.com>
2021-02-11 22:05:29 +0530

    added endpoints for login and employee addition

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,18 +1,35 @@
 const Home = {"Tables": 10,"Parcels":9,"Kitchens": 10,"Waiters": 14,"Billers":10,"Customers":30};
+//const employeeData = {"Type": "Waiters","Name": "abc","Email": "abc@gmail.com", "Mobile" : "8280116464","Username": "abc","Password": "abc","DOJ": "20.11.15","status": 200,"msg":"employee added successfully"}
+const Loginmsg = {"status" : 200 ,"msg": "login successful"}
+const employeemsg = {"msg": "employee added", "status": 200}
+//const dummyData = {"Email": "abc@gmail.com", "password": "abc"}
 
 const routes = (app) => {
-
     app.route('/homepagedata').get((req,res) => res.send(Home)).post((req, res) =>
     res.send('POST request successful!'));
-     // create a new route so you can get these donation entries by their ID's
-  app.route('/home/:homeID')
-  //create put request
-  .put((req, res) =>
-  res.send('PUT request successful!'))
-  //create delete request
-  .delete((req, res) =>
-  res.send('DELETE request successful'))
 
+  // app.route('/login').post((req,res) => 
+  // res.send(msg))
+  //similar to above syntax
+  // app.post('/login',(req,res) => 
+  // res.send(msg))
+
+  app.route('/login').post((req,res) => {
+    //  let Email = req.body.email;
+    //  let password = req.body.password;
+    let {email,password} = req.body;
+    res.send(Loginmsg);
+    //console.log("Email is = "+email+", password is  "+password);
+    //console.log(req.body);
+  })
+
+  app.route('/addemployee').post((req,res) => {
+    //let data = employeeData;
+    //console.log("all employee data = "+data+"");
+    let {Type,Name,Email,Mobile,Username,Password,DOJ} = req.body;
+    //console.log(req.body)
+  res.send(employeemsg)
+  })
 }
 
 module.exports = routes;
