857c9b5be0d6bc93911b23d7bc546ebbebeee24c
AnilKSahoo <asahoo112@gmail.com>
2021-02-17 09:47:19 +0530

    mongodb connected

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const routes = require('./routes/routes');
-//const mongoose = require('mongoose');
-const bodyParser = require('body-Parser');
+const express = require("express");
+const routes = require("./routes/routes");
+const mongoose = require("mongoose");
+const bodyParser = require("body-Parser");
 const cors = require("cors");
 const app = express();
 const PORT = 4000;
@@ -14,22 +14,42 @@ const PORT = 4000;
 // })
 
 //bodyparser setup
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(cors());
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 
 routes(app);
 
 //for checking whether localhost:4000/ is working in chrome tab or not
-app.get('/', (req, res) =>
+app.get("/", (req, res) =>
   res.send(`Node and express server running on port ${PORT}`)
-)
+);
 
-app.listen(PORT, () =>
-console.log(`Your server is running on port ${PORT}`))
+mongoose
+  .connect(
+    // "mongodb+srv://Devdatabase:GetDevconnection@cluster0.b9nxv.mongodb.net/GetDevs?retryWrites=true&w=majority",
+    "mongodb+srv://Devdatabase:GetDevconnection@cluster0.b9nxv.mongodb.net/restaurant?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    // app.listen(4000);
+    console.log("db connected...!");
+    app.listen(PORT, () =>
+      console.log(`Your server is running on port ${PORT}`)
+    );
+  })
+  .catch((err) => {
+    console.log(err);
+  });
