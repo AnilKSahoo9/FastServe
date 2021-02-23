31de05e46a03a9dd1ad9a3b3335c439fcb2083ce
sweta222 <swetastuti2@gmail.com>
2021-02-23 13:44:47 +0530

    data inside modal issue got partially cleared

--- client/src/App.js
+++ client/src/App.js
@@ -1,42 +1,44 @@
-// import logo from './logo.svg';
-// import './App.css';
-// import {BrowserRouter as Router, Route,Link} from "react-router-dom"
+import logo from './logo.svg';
+import './App.css';
+import {BrowserRouter as Router, Route,Link} from "react-router-dom"
 
-// import Header from './components/Header';
-// import { Col, Container, Row, Toast} from 'reactstrap';
-// import {ToastContainer} from 'react-toastify';
-// import { Menu } from './components/Menu';
-// import Home from './components/Home';
-// import AddUser from './components/AddUser';
-// import Showuser from './components/Showuser';
-// function App() {
-//   return (
-//     <div className="App">
-//     <Router>
-//     {/* <ToastContainer /> */}
-//     {/* <Container  className="ml-0 mr-0"> */}
-//     <Header />
-//     <Row >
-//       <Col md={4}>
-//        <Menu />
-//       </Col>
-//       <Col md={8}>
-//       <Route path="/" component={Home} exact />
-//       <Route path="/AddUser" component={AddUser} exact />
-//       <Route path="/Showuser" component={Showuser} exact />
-//       </Col>
-//     </Row>
-//     {/* </Container> */}
-//     </Router>
-//    </div>
-//   );
-//  }
-// export default App;
-
-import React from "react";
-import "./App.css";
+import Header from './components/Header';
+import { Col, Container, Row, Toast} from 'reactstrap';
+import {ToastContainer} from 'react-toastify';
+import { Menu } from './components/Menu';
+import Home from './components/Home';
+import AddUser from './components/AddUser';
+import Showuser from './components/Showuser';
 import AdminDashboard from "./pages/adminDashboard.js";
-const App = () => {
-  return <AdminDashboard />;
-};
+function App() {
+  return (
+    <div className="App">
+    <Router>
+    {/* <ToastContainer /> */}
+    {/* <Container  className="ml-0 mr-0"> */}
+    <Header />
+    <Row >
+      <Col md={4}>
+       <Menu />
+      </Col>
+      <Col md={8}>
+      <Route path="/" component={Home} exact />
+      <Route path="/AddUser" component={AddUser}  />
+      <Route path="/Showuser" component={Showuser}  />
+      <Route path="/adminDashboard" component={AdminDashboard}  />
+      </Col>
+    </Row>
+    {/* </Container> */}
+    </Router>
+   </div>
+  );
+ }
 export default App;
+
+// import React from "react";
+// import "./App.css";
+// import AdminDashboard from "./pages/adminDashboard.js";
+// const App = () => {
+//   return <AdminDashboard />;
+// };
+// export default App;
--- client/src/components/Showuser.js
+++ client/src/components/Showuser.js
@@ -1,157 +1,319 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button, Table } from 'reactstrap';
-const Showuser = () => {
+import ModalExample from './modal';
+const Showuser = (props) => {
+    const [UN,setUN] = useState("Ravi Kumar");
     return (
         <div >
-        <div className="border border-dark rounded ml-4 mr-4 mb-3">
-            <div style={{ backgroundColor: "Gray", color: "white " }} ><h4>Waiter</h4></div>
-            <div>
-            <Table responsive size="sm">
-                <thead >
-                    <tr style={{width: "30%"}}>
-                        <th>No.</th>
-                        <th>User Name</th>
-                        <th>Attendance for Today</th>
-                        <th>Details</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>Ravi Kumar</td>
-                        <td>Present</td>
-                        <td><Button outline color="primary">Show Details</Button></td>
-                    </tr>
-                    <tr>
-                        <th scope="row">2</th>
-                        <td>Pranaya Gulati</td>
-                        <td>Present</td>
-                        <td><Button outline color="primary">Show Details</Button></td>
-                    </tr>
-                    <tr>
-                        <th scope="row">3</th>
-                        <td>Ankur Tripathi</td>
-                        <td>Half-Day</td>
-                        <td><Button  outline color="primary">Show Details</Button></td>
-                    </tr>
-                    
-                </tbody> 
-               
-            </Table>
+            <div className="border border-dark rounded ml-4 mr-4 mb-3">
+                <div style={{ backgroundColor: "Gray", color: "white " }} ><h4>Waiter</h4></div>
+                <div>
+                    <Table responsive size="sm">
+                        <thead >
+                            <tr style={{ width: "30%" }}>
+                                <th>No.</th>
+                                <th>User Name</th>
+                                <th>Attendance for Today</th>
+                                <th>Details</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>{UN}</td>
+                                <td>Present</td>
+                                <td>
+                                <ModalExample USERNAME={UN}
+                                /> 
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">2</th>
+                                <td>Pranaya Gulati</td>
+                                <td>Present</td>
+                                <td><ModalExample /></td>
+                            </tr>
+                            <tr>
+                                <th scope="row">3</th>
+                                <td>Ankur Tripathi</td>
+                                <td>Half-Day</td>
+                                <td><ModalExample /></td>
+                            </tr>
+
+                        </tbody>
+
+                    </Table>
+                </div>
             </div>
-           </div>
-           <div className="border border-dark rounded ml-4 mr-4 mb-3">
-            <div style={{backgroundColor: "Gray", color: "white " }}><h4>Billers</h4></div>
-            
-            <div>
-            <Table responsive size="sm">
-                <thead>
-                    <tr>
-                        <th>No.</th>
-                        <th>User Name</th>
-                        <th>Attendance for Today</th>
-                        <th>Details</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>Ranvijaya Rastogi</td>
-                        <td>Present</td>
-                        <td><Button outline color="primary">Show Details</Button></td>
-                    </tr>
-                    <tr>
-                        <th scope="row">2</th>
-                        <td>Akhilesh Singh</td>
-                        <td>Absent</td>
-                        <td><Button outline color="primary">Show Details</Button></td>
-                    </tr>
-                    <tr>
-                        <th scope="row">3</th>
-                        <td>Suresh Mishra</td>
-                        <td>Present</td>
-                        <td><Button outline color="primary">Show Details</Button></td>
-                    </tr>
-                </tbody>
-            </Table>
+            <div className="border border-dark rounded ml-4 mr-4 mb-3">
+                <div style={{ backgroundColor: "Gray", color: "white " }}><h4>Billers</h4></div>
+
+                <div>
+                    <Table responsive size="sm">
+                        <thead>
+                            <tr>
+                                <th>No.</th>
+                                <th>User Name</th>
+                                <th>Attendance for Today</th>
+                                <th>Details</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>Ranvijaya Rastogi</td>
+                                <td>Present</td>
+                                <td><ModalExample /></td>
+                            </tr>
+                            <tr>
+                                <th scope="row">2</th>
+                                <td>Akhilesh Singh</td>
+                                <td>Absent</td>
+                                <td><ModalExample /></td>
+                            </tr>
+                            <tr>
+                                <th scope="row">3</th>
+                                <td>Suresh Mishra</td>
+                                <td>Present</td>
+                                <td><ModalExample /></td>
+                            </tr>
+                        </tbody>
+                    </Table>
+                </div>
             </div>
-            </div>
-           
+
 
             <div className="border border-dark rounded ml-4 mr-4 mb-3 ">
-            <div style={{backgroundColor: "Gray", color: "white " }}><h4>Kichens</h4></div>
-            <div >
-            <Table responsive size="sm">
-                <thead>
-                    <tr>
-                        <th>No.</th>
-                        <th>User Name</th>
-                        <th>Attendance for Today</th>
-                        <th>Details</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>Ruturaj Agrawal</td>
-                        <td>Present</td>
-                        <td><Button outline color="primary">Show Details</Button></td>
-                    </tr>
-                    <tr>
-                        <th scope="row">2</th>
-                        <td>Aswin Panigrahi</td>
-                        <td>Present</td>
-                        <td><Button outline color="primary">Show Details</Button></td>
-                    </tr>
-                    <tr>
-                        <th scope="row">3</th>
-                        <td>Ashish Sahoo</td>
-                        <td>Present</td>
-                        <td><Button outline color="primary">Show Details</Button></td>
-                    </tr>
-                </tbody>
-            </Table>
+                <div style={{ backgroundColor: "Gray", color: "white " }}><h4>Kichens</h4></div>
+                <div >
+                    <Table responsive size="sm">
+                        <thead>
+                            <tr>
+                                <th>No.</th>
+                                <th>User Name</th>
+                                <th>Attendance for Today</th>
+                                <th>Details</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>Ruturaj Agrawal</td>
+                                <td>Present</td>
+                                <td><Button outline color="primary">Show Details</Button></td>
+                            </tr>
+                            <tr>
+                                <th scope="row">2</th>
+                                <td>Aswin Panigrahi</td>
+                                <td>Present</td>
+                                <td><Button outline color="primary">Show Details</Button></td>
+                            </tr>
+                            <tr>
+                                <th scope="row">3</th>
+                                <td>Ashish Sahoo</td>
+                                <td>Present</td>
+                                <td><Button outline color="primary">Show Details</Button></td>
+                            </tr>
+                        </tbody>
+                    </Table>
+                </div>
             </div>
-            </div>
-            
+
             <div className="border border-dark rounded ml-4 mr-4 mb-3">
-            <div style={{ backgroundColor: "Gray", color: "white " }}><h4>Other Workers</h4></div>
-            <div >
-            <Table responsive size="sm">
-                <thead>
-                    <tr>
-                        <th>No.</th>
-                        <th>User Name</th>
-                        <th>Attendance for Today</th>
-                        <th>Details</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>Rajesh kumar</td>
-                        <td>Present</td>
-                        <td><Button outline color="primary">Show Details</Button></td>
-                    </tr>
-                    <tr>
-                        <th scope="row">2</th>
-                        <td>Juned Raj</td>
-                        <td>Present</td>
-                        <td><Button outline color="primary">Show Details</Button></td>
-                    </tr>
-                    <tr>
-                        <th scope="row">3</th>
-                        <td>Satu Swain</td>
-                        <td>Present</td>
-                        <td><Button outline color="primary">Show Details</Button></td>
-                    </tr>
-                </tbody>
-            </Table>
+                <div style={{ backgroundColor: "Gray", color: "white " }}><h4>Other Workers</h4></div>
+                <div >
+                    <Table responsive size="sm">
+                        <thead>
+                            <tr>
+                                <th>No.</th>
+                                <th>User Name</th>
+                                <th>Attendance for Today</th>
+                                <th>Details</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>Rajesh kumar</td>
+                                <td>Present</td>
+                                <td><Button outline color="primary">Show Details</Button></td>
+                            </tr>
+                            <tr>
+                                <th scope="row">2</th>
+                                <td>Juned Raj</td>
+                                <td>Present</td>
+                                <td><Button outline color="primary">Show Details</Button></td>
+                            </tr>
+                            <tr>
+                                <th scope="row">3</th>
+                                <td>Satu Swain</td>
+                                <td>Present</td>
+                                <td><Button outline color="primary">Show Details</Button></td>
+                            </tr>
+                        </tbody>
+                    </Table>
+                </div>
             </div>
-            </div>
-            </div>
-        
+        </div>
+
     )
 }
 
-export default Showuser
+export default Showuser;
+// import React from 'react'
+// import { Button, Table } from 'reactstrap';
+// const Showuser = () => {
+//     return (
+//         <div >
+//         <div className="border border-dark rounded ml-4 mr-4 mb-3">
+//             <div style={{ backgroundColor: "Gray", color: "white " }} ><h4>Waiter</h4></div>
+//             <div>
+//             <Table responsive size="sm">
+//                 <thead >
+//                     <tr style={{width: "30%"}}>
+//                         <th>No.</th>
+//                         <th>User Name</th>
+//                         <th>Attendance for Today</th>
+//                         <th>Details</th>
+//                     </tr>
+//                 </thead>
+//                 <tbody>
+//                     <tr>
+//                         <th scope="row">1</th>
+//                         <td>Ravi Kumar</td>
+//                         <td>Present</td>
+//                         <td><Button outline color="primary">Show Details</Button></td>
+//                     </tr>
+//                     <tr>
+//                         <th scope="row">2</th>
+//                         <td>Pranaya Gulati</td>
+//                         <td>Present</td>
+//                         <td><Button outline color="primary">Show Details</Button></td>
+//                     </tr>
+//                     <tr>
+//                         <th scope="row">3</th>
+//                         <td>Ankur Tripathi</td>
+//                         <td>Half-Day</td>
+//                         <td><Button  outline color="primary">Show Details</Button></td>
+//                     </tr>
+                    
+//                 </tbody> 
+               
+//             </Table>
+//             </div>
+//            </div>
+//            <div className="border border-dark rounded ml-4 mr-4 mb-3">
+//             <div style={{backgroundColor: "Gray", color: "white " }}><h4>Billers</h4></div>
+            
+//             <div>
+//             <Table responsive size="sm">
+//                 <thead>
+//                     <tr>
+//                         <th>No.</th>
+//                         <th>User Name</th>
+//                         <th>Attendance for Today</th>
+//                         <th>Details</th>
+//                     </tr>
+//                 </thead>
+//                 <tbody>
+//                     <tr>
+//                         <th scope="row">1</th>
+//                         <td>Ranvijaya Rastogi</td>
+//                         <td>Present</td>
+//                         <td><Button outline color="primary">Show Details</Button></td>
+//                     </tr>
+//                     <tr>
+//                         <th scope="row">2</th>
+//                         <td>Akhilesh Singh</td>
+//                         <td>Absent</td>
+//                         <td><Button outline color="primary">Show Details</Button></td>
+//                     </tr>
+//                     <tr>
+//                         <th scope="row">3</th>
+//                         <td>Suresh Mishra</td>
+//                         <td>Present</td>
+//                         <td><Button outline color="primary">Show Details</Button></td>
+//                     </tr>
+//                 </tbody>
+//             </Table>
+//             </div>
+//             </div>
+           
+
+//             <div className="border border-dark rounded ml-4 mr-4 mb-3 ">
+//             <div style={{backgroundColor: "Gray", color: "white " }}><h4>Kichens</h4></div>
+//             <div >
+//             <Table responsive size="sm">
+//                 <thead>
+//                     <tr>
+//                         <th>No.</th>
+//                         <th>User Name</th>
+//                         <th>Attendance for Today</th>
+//                         <th>Details</th>
+//                     </tr>
+//                 </thead>
+//                 <tbody>
+//                     <tr>
+//                         <th scope="row">1</th>
+//                         <td>Ruturaj Agrawal</td>
+//                         <td>Present</td>
+//                         <td><Button outline color="primary">Show Details</Button></td>
+//                     </tr>
+//                     <tr>
+//                         <th scope="row">2</th>
+//                         <td>Aswin Panigrahi</td>
+//                         <td>Present</td>
+//                         <td><Button outline color="primary">Show Details</Button></td>
+//                     </tr>
+//                     <tr>
+//                         <th scope="row">3</th>
+//                         <td>Ashish Sahoo</td>
+//                         <td>Present</td>
+//                         <td><Button outline color="primary">Show Details</Button></td>
+//                     </tr>
+//                 </tbody>
+//             </Table>
+//             </div>
+//             </div>
+            
+//             <div className="border border-dark rounded ml-4 mr-4 mb-3">
+//             <div style={{ backgroundColor: "Gray", color: "white " }}><h4>Other Workers</h4></div>
+//             <div >
+//             <Table responsive size="sm">
+//                 <thead>
+//                     <tr>
+//                         <th>No.</th>
+//                         <th>User Name</th>
+//                         <th>Attendance for Today</th>
+//                         <th>Details</th>
+//                     </tr>
+//                 </thead>
+//                 <tbody>
+//                     <tr>
+//                         <th scope="row">1</th>
+//                         <td>Rajesh kumar</td>
+//                         <td>Present</td>
+//                         <td><Button outline color="primary">Show Details</Button></td>
+//                     </tr>
+//                     <tr>
+//                         <th scope="row">2</th>
+//                         <td>Juned Raj</td>
+//                         <td>Present</td>
+//                         <td><Button outline color="primary">Show Details</Button></td>
+//                     </tr>
+//                     <tr>
+//                         <th scope="row">3</th>
+//                         <td>Satu Swain</td>
+//                         <td>Present</td>
+//                         <td><Button outline color="primary">Show Details</Button></td>
+//                     </tr>
+//                 </tbody>
+//             </Table>
+//             </div>
+//             </div>
+//             </div>
+        
+//     )
+// }
+
+// export default Showuser
