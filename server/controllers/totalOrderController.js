ee716f9f3af8a59902855089bf79325ce85e11d2
sweta222 <swetastuti2@gmail.com>
2021-03-06 11:39:06 +0530

    tried aggregate function for performing left outer join operation on both tables and sessions

--- server/controllers/totalOrderController.js
+++ server/controllers/totalOrderController.js
@@ -2,15 +2,34 @@ let tableSchema = require("../models/tableModel");
 let sessionSchema = require("../models/sessionModel");
 
 const totalOrderController = (req,res) => {
-    let arraytables = [];
-tableSchema.find({},(err,doc) => {
-    doc.map((elem) => {
-        elem.session.map((session_id) => {
-            session_id ? arraytables.push({tableNo:elem.tableNo,sessions:elem.session}) : null
-        })
-    })
-    res.send(arraytables);
-});
+//     let arraytables = [];
+// tableSchema.find({},(err,doc) => {
+//     doc.map((elem) => {
+//         elem.session.map((session_id) => {
+//             session_id ? arraytables.push({tableNo:elem.tableNo,sessions:elem.session}) : null;
+//             //sessionSchema.findById({_id:session_id},(err,result) => {console.log(result)})
+//         })
+//     })
+//     res.send(arraytables);
+// });
+tableSchema.aggregate([{
+    $lookup:{
+        from:'sessions',
+        localField:'session',
+        foreignField:'_id',
+        as:'totalOrder'
+    }
+}
+],(err,data) => { return res.json(data)}
+)
+//res.send('ok')
+};
+module.exports = totalOrderController;
+
+// .toArray((err,res) => {
+//     if(err) throw err;
+//     console.log(JSON.stringify(res));
+// })
 
     // let arraytables = [];
     // tableSchema.find({}, function (err, tables) {
@@ -43,14 +62,16 @@ tableSchema.find({},(err,doc) => {
     // // tableSchema.find();
     // // console.log(totalorder);
     // // res.send("success");
-}
-module.exports = totalOrderController;
 
-// let arraytables = [];
+
+// const totalOrderController = (req,res) => {
+//     let arraytables = [];
 // tableSchema.find({},(err,doc) => {
 //     doc.map((elem) => {
 //         elem.session.map((session_id) => {
-//             session_id ? arraytables.push({tableNo:elem,sessions:elem.session}) : null
+//             session_id ? arraytables.push({tableNo:elem.tableNo,sessions:elem.session}) : null;
+//             //sessionSchema.findById({_id:session_id},(err,result) => {console.log(result)})
 //         })
 //     })
-// })
+//     res.send(arraytables);
+// });
