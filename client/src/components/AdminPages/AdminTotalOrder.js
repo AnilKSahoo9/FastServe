3cebf5d116bbbc7deeb9ee515a7f448bffe7ca12
sweta222 <swetastuti2@gmail.com>
2021-03-25 13:34:44 +0530

    real time sync completed inside useeffect

--- client/src/components/AdminPages/AdminTotalOrder.js
+++ client/src/components/AdminPages/AdminTotalOrder.js
@@ -10,7 +10,6 @@ const AdminTotalOrder = () => {
   const [responseData,setResponseData] = useState([]);
   const [tableOrders,setTableOrders] = useState([]);
   const [parcelOrders,setParcelOrders] = useState([]);
-  const [newlyAddedParcelData,setNAPD] = useState([]);
   const ENDPOINT = 'ws://localhost:4000/api/socket';
   let connectionOptions = 
     {
@@ -20,38 +19,29 @@ const AdminTotalOrder = () => {
       "transports" : ["websocket"]
   };
   
-  const fetchData = () => {
-    axios.get(`http://localhost:4000/admin-totalorders/`).then((response) => {
+  const fetchData = async() => {
+    const response = await axios.get(`http://localhost:4000/admin-totalorders/`)
       setResponseData(response.data);
       setTableOrders(response.data.tableOrders);
       setParcelOrders(response.data.parcelOrders);
-    })
   }
-
-useEffect(() => {
-  fetchData();
-},[]);
 
 useEffect(() => {
   socket = io(ENDPOINT,connectionOptions);
   //console.log(socket);
-// socket.on('news',(data) => {
-//   console.log(data);
-//   socket.emit('my other event',{my:'data'});
-// });
-
-socket.on('parcelData',(parcelData) => {
-  console.log(parcelData);
-  setNAPD(parcelData);
-  //parcelOrders.push(parcelData);
+ socket.on('parcelData',(parcelData) => {
+  //console.log(parcelData);
+  let parcelData2 = {};
+  parcelData2 = parcelData;
+  parcelOrders.push(parcelData2);
   socket.emit('my',{name:'sweta'})
 });
-
   // return () => {
   //   socket.emit("disconnect");
   //   socket.off();
   // }
-},[ENDPOINT,parcelOrders,newlyAddedParcelData])
+  fetchData();
+},[parcelOrders,ENDPOINT])
 
   return (
     <div
@@ -170,8 +160,7 @@ socket.on('parcelData',(parcelData) => {
         </Col>
         <Col md={6} xs={12}>
           <h2>-:Parcel Details:-</h2>
-          {parcelOrders || newlyAddedParcelData ? (<div>
-          {/* <div> */}
+          {/* {console.log(parcelOrders)} */}
           {parcelOrders.map((val,index) =>
               <Accordion
                 style={{
@@ -186,7 +175,7 @@ socket.on('parcelData',(parcelData) => {
                       eventKey={String(index)}
                       style={{ backgroundColor: "#f3e6ff" }}
                     >
-                      <h5>ParcelNo:{val.parcelNo}</h5>
+                      <h5>ParcelNo:</h5>
                     </Accordion.Toggle>
                   </Card.Header>
                   <Accordion.Collapse eventKey={String(index)}>
@@ -208,7 +197,7 @@ socket.on('parcelData',(parcelData) => {
                           </tr>
                         </thead>
 
-                        {val.orderDetails.map((valueitem, index) => (
+                        {val.items.map((valueitem, index) => (
                           <tbody>
                             <tr>
                               <td
@@ -253,97 +242,8 @@ socket.on('parcelData',(parcelData) => {
                   </Accordion.Collapse>
                 </Card>
               </Accordion>
-            
           )}
-          {/* </div> */}
-
-          {/* <div> */}
-          {newlyAddedParcelData.map((item,index2) => 
-          <Accordion style={{
-            boxShadow: "5px 10px 20px 5px rgba(0, 0, 0, 0.253)",
-            borderRadius: "0.10rem",
-          }}>
-          <Card>
-          <Card.Header style={{ backgroundColor: "#e6ccff" }}>
-                    <Accordion.Toggle
-                      as={Card.Header}
-                      eventKey={String(index2)}
-                      style={{ backgroundColor: "#f3e6ff" }}
-                    >
-                      <h5>ParcelNo:{item.parcelNo}</h5>
-                    </Accordion.Toggle>
-            </Card.Header>
-            <Accordion.Collapse eventKey={String(index2)}>
-            <Card.Body>
-                      <h5>Order delivered by {item.billerName}</h5>
-                      <Table striped bordered hover>
-                        <thead>
-                          <tr>
-                            <th style={{ backgroundColor: "#ffe6ff" }}>
-                              Sl no.
-                            </th>
-                            <th style={{ backgroundColor: "#ffe6ff" }}>Item</th>
-                            <th style={{ backgroundColor: "#ffe6ff" }}>
-                              Quantity
-                            </th>
-                            <th style={{ backgroundColor: "#ffe6ff" }}>
-                              Price
-                            </th>
-                          </tr>
-                        </thead>
-
-                        {item.orderDetails.map((valueitem, index3) => (
-                          <tbody>
-                            <tr>
-                              <td
-                                style={{
-                                  backgroundColor: "white",
-                                }}
-                              >
-                                {index3 + 1}
-                              </td>
-                              <td
-                                style={{
-                                  backgroundColor: "white",
-                                }}
-                              >
-                                {valueitem.name}
-                              </td>
-                              <td
-                                style={{
-                                  backgroundColor: "white",
-                                }}
-                              >
-                                {valueitem.quantity}
-                              </td>
-                              <td
-                                style={{
-                                  backgroundColor: "white",
-                                }}
-                              >
-                                {valueitem.price}
-                              </td>
-                            </tr>
-                            <tr>
-                                  <th style={{ backgroundColor: "#ffe6ff" }}>
-                                    Total:
-                                  </th>
-                                  <td colspan="3">{valueitem.quantity * valueitem.price}</td>
-                                </tr>
-                          </tbody>
-                        ))}
-                      </Table>
-                    </Card.Body>
-            </Accordion.Collapse>
-          </Card>
-
-          </Accordion>
-          )}
-          {/* </div>  */}
-          
-          </div>
-          ) : null
-          }</Col>
+          </Col>
       </Row>
     </div>
   );
