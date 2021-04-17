ae37c0745a6e9f758ae18971f5d7eaa5a499db4b
sweta222 <swetastuti2@gmail.com>
2021-04-17 09:28:18 +0530

    debugging admin-home test-1

--- server/controllers/homepagedataController.js
+++ server/controllers/homepagedataController.js
@@ -8,7 +8,7 @@ const homepagedata = async(req,res) => {
   let parcelOrdersTillNow = [];
   let totalWaitersWorking = [];
   let acceptedTableOrders = [];
-
+  var TAT;
   await sessionSchema.find({billStatus:"unpaid",created_at:new Date().toISOString().replace('-','/').split('T')[0].replace('-','/')},(err,doc) => {
     if(err){
       res.status(500).json({msg:"failed"});
@@ -52,18 +52,33 @@ await employeeSchema.find({status:"present"},(err,doc) => {
 
 await sessionSchema.find({orderStatus:"accept",created_at:new Date().toISOString().replace('-','/').split('T')[0].replace('-','/')},(err,doc) => {
   if(doc){
+    //console.log(doc)
     acceptedTableOrders = doc;
     let totalAmountTable = doc.map((item) => item.totalAmount);
-    let TAT = totalAmountTable.reduce((a,b) => {return a + b});
-    parcelSchema.find({created_at:new Date().toISOString().replace('-','/').split('T')[0].replace('-','/'),orderStatus:"accept"},(err2,doc2) => {
+   TAT = totalAmountTable.reduce((a,b) => {return a + b});
+    // parcelSchema.find({created_at:new Date().toISOString().replace('-','/').split('T')[0].replace('-','/'),orderStatus:"accept"},(err2,doc2) => {
+    //   if(doc2){
+    //     let totalAmountParcel = doc2.map((item) => item.totalAmount);
+    //     let TAP = totalAmountParcel.reduce((a,b) => {return a + b ;});
+    //     //console.log(TAP)
+    //     res.status(200).json({"tables active":tablesActive.length,"tableorderstillnow":tableOrdersTillNow.length,"parcelorderstillnow":parcelOrdersTillNow.length,"totalwaitersworking":totalWaitersWorking.length,"totalordersacceptedbykitchen":acceptedTableOrders.length + doc2.length,"totalamountcollected":TAP + TAT});
+    //   }
+    // });
+  }
+  else{
+    return res.status(500).json({msg:"error occured"});
+  }
+});
+
+parcelSchema.find({created_at:new Date().toISOString().replace('-','/').split('T')[0].replace('-','/'),orderStatus:"accept"},(err2,doc2) => {
       if(doc2){
         let totalAmountParcel = doc2.map((item) => item.totalAmount);
         let TAP = totalAmountParcel.reduce((a,b) => {return a + b ;});
-        //console.log(TAP)
         res.status(200).json({"tables active":tablesActive.length,"tableorderstillnow":tableOrdersTillNow.length,"parcelorderstillnow":parcelOrdersTillNow.length,"totalwaitersworking":totalWaitersWorking.length,"totalordersacceptedbykitchen":acceptedTableOrders.length + doc2.length,"totalamountcollected":TAP + TAT});
       }
+      else{
+        return res.status(500).json({msg:"error occured"});
+      }
     });
-  }
-});
 }
 module.exports = homepagedata;
