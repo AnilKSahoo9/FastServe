daf544bf359db6bbb533a38fb36bbce799e4f88e
sweta222 <swetastuti2@gmail.com>
2021-05-02 16:13:10 +0530

    added created_at into table schema

--- server/models/tableModel.js
+++ server/models/tableModel.js
@@ -3,5 +3,6 @@ const tableSchema = new mongoose.Schema({
   tableNo: { type: Number },
   session: { type: Array },
   tableStatus: { type: String },
+  created_at:{type: String}
 });
 module.exports = mongoose.model("tables", tableSchema);
