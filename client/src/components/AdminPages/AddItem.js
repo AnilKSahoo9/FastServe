e9c056c935cd85f2683932d469592c7aad71bca0
arati14 <pradhan.arati14@gmail.com>
2021-05-05 20:18:55 +0530

    deleted the notification.css and admin.css

--- client/src/components/AdminPages/AddItem.js
+++ client/src/components/AdminPages/AddItem.js
@@ -19,8 +19,6 @@ import dessert from "./image/ic.jpg";
 import bevergaes from "./image/drink.jpg";
 import rice from "./image/fried-rice.jpg";
 import chicken from "./image/chicken.jpg";
-import "../../css/admin.css";
-// import "../AdminPages/AdminStyles.css";
 import "../../css/AdminStyles.css";
 const AddItem = () => {
   const [modalShow, setModalShow] = useState(false);
@@ -146,238 +144,249 @@ const AddItem = () => {
   console.log(arrData);
   return (
     <div className="inner-container">
-    <div className="admin_additems">
-      <div style={{ float: "right" }}>
-        {" "}
-        <Button 
-        className="additem_btnstyle"
-          size="lg"
-          onClick={() => setModalShow(true)}
-          // active
-        >
-          Add Item {"  "}
-          <FontAwesomeIcon icon={faPlus} />
-        </Button>
-        <Modal
-          show={modalShow}
-          onHide={handleClose}
-          size="lg"
-          aria-labelledby="contained-modal-title-vcenter"
-          centered
-        >
-          <Modal.Header closeButton>
-            <Modal.Title id="contained-modal-title-vcenter">
-              Item Details
-            </Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <DropdownButton
-              id="dropdown-basic-button"
-              title={value}
-              onSelect={handleSelect}
-              value={value}
-            >
-              <Dropdown.Item eventKey="Starters">Starters</Dropdown.Item>
-              <Dropdown.Item eventKey="Chinese">Chinese</Dropdown.Item>
-              <Dropdown.Item eventKey="Veg Main Course">
-                Veg Main Course
-              </Dropdown.Item>
-              <Dropdown.Item eventKey="Drink">Drink</Dropdown.Item>
-              <Dropdown.Item eventKey="Salad">Salad</Dropdown.Item>
-              <Dropdown.Item eventKey="Dessert">Dessert</Dropdown.Item>
-              <Dropdown.Item eventKey="Non-Veg Main Course">
-                Non-Veg Main Course
-              </Dropdown.Item>
-            </DropdownButton>
-            {value === "Non-Veg Main Course" ? (
+      <div className="admin_additems">
+        <div style={{ float: "right" }}>
+          {" "}
+          <Button
+            className="additem_btnstyle"
+            size="lg"
+            onClick={() => setModalShow(true)}
+            // active
+          >
+            Add Item {"  "}
+            <FontAwesomeIcon icon={faPlus} />
+          </Button>
+          <Modal
+            show={modalShow}
+            onHide={handleClose}
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+          >
+            <Modal.Header closeButton>
+              <Modal.Title id="contained-modal-title-vcenter">
+                Item Details
+              </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
               <DropdownButton
                 id="dropdown-basic-button"
-                title={value1}
-                onSelect={handleSelect1}
-                value={value1}
+                title={value}
+                onSelect={handleSelect}
+                value={value}
               >
-                <Dropdown.Item eventKey="Chicken">Chicken</Dropdown.Item>
-                <Dropdown.Item eventKey="Mutton">Mutton</Dropdown.Item>
-                <Dropdown.Item eventKey="Fish">Fish</Dropdown.Item>
+                <Dropdown.Item eventKey="Starters">Starters</Dropdown.Item>
+                <Dropdown.Item eventKey="Chinese">Chinese</Dropdown.Item>
+                <Dropdown.Item eventKey="Veg Main Course">
+                  Veg Main Course
+                </Dropdown.Item>
+                <Dropdown.Item eventKey="Drink">Drink</Dropdown.Item>
+                <Dropdown.Item eventKey="Salad">Salad</Dropdown.Item>
+                <Dropdown.Item eventKey="Dessert">Dessert</Dropdown.Item>
+                <Dropdown.Item eventKey="Non-Veg Main Course">
+                  Non-Veg Main Course
+                </Dropdown.Item>
               </DropdownButton>
-            ) : value === "Veg Main Course" ? (
-              <DropdownButton
-                id="dropdown-basic-button"
-                title={value2}
-                onSelect={handleSelect2}
-                value={value2}
+              {value === "Non-Veg Main Course" ? (
+                <DropdownButton
+                  id="dropdown-basic-button"
+                  title={value1}
+                  onSelect={handleSelect1}
+                  value={value1}
+                >
+                  <Dropdown.Item eventKey="Chicken">Chicken</Dropdown.Item>
+                  <Dropdown.Item eventKey="Mutton">Mutton</Dropdown.Item>
+                  <Dropdown.Item eventKey="Fish">Fish</Dropdown.Item>
+                </DropdownButton>
+              ) : value === "Veg Main Course" ? (
+                <DropdownButton
+                  id="dropdown-basic-button"
+                  title={value2}
+                  onSelect={handleSelect2}
+                  value={value2}
+                >
+                  <Dropdown.Item eventKey="Paneer">Paneer</Dropdown.Item>
+                  <Dropdown.Item eventKey="Mushroom">Mushroom</Dropdown.Item>
+                  <Dropdown.Item eventKey="Fish">Fish</Dropdown.Item>
+                </DropdownButton>
+              ) : (
+                ""
+              )}
+
+              <InputGroup style={{ marginTop: "1rem" }}>
+                <InputGroup.Prepend>
+                  <InputGroup.Text>Item Name :</InputGroup.Text>
+                </InputGroup.Prepend>
+                <FormControl
+                  as="textarea"
+                  aria-label="With textarea"
+                  onChange={handleChange}
+                  value={itemName}
+                />
+              </InputGroup>
+
+              <InputGroup style={{ marginTop: "1rem" }}>
+                <InputGroup.Prepend>
+                  <InputGroup.Text>Item Price :</InputGroup.Text>
+                </InputGroup.Prepend>
+                <FormControl
+                  as="textarea"
+                  aria-label="With textarea"
+                  onChange={handleChangePrice}
+                  value={itemPrice}
+                />
+              </InputGroup>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button
+                className="additem_btnstyle"
+                type="submit"
+                onClick={addMoreItem}
               >
-                <Dropdown.Item eventKey="Paneer">Paneer</Dropdown.Item>
-                <Dropdown.Item eventKey="Mushroom">Mushroom</Dropdown.Item>
-                <Dropdown.Item eventKey="Fish">Fish</Dropdown.Item>
-              </DropdownButton>
-            ) : (
-              ""
-            )}
-
-            <InputGroup style={{ marginTop: "1rem" }}>
-              <InputGroup.Prepend>
-                <InputGroup.Text>Item Name :</InputGroup.Text>
-              </InputGroup.Prepend>
-              <FormControl
-                as="textarea"
-                aria-label="With textarea"
-                onChange={handleChange}
-                value={itemName}
-              />
-            </InputGroup>
-
-            <InputGroup style={{ marginTop: "1rem" }}>
-              <InputGroup.Prepend>
-                <InputGroup.Text>Item Price :</InputGroup.Text>
-              </InputGroup.Prepend>
-              <FormControl
-                as="textarea"
-                aria-label="With textarea"
-                onChange={handleChangePrice}
-                value={itemPrice}
-              />
-            </InputGroup>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button className="additem_btnstyle" type="submit" onClick={addMoreItem}>
-              Add more item
-            </Button>
-            <Button className="additem_btnstyle"type="submit" onClick={mySubmitHandler}>
-              Submit
-            </Button>
-          </Modal.Footer>
-        </Modal>
-      </div>
-
-      <div>
-        <h2 className="heading"><b>Menu Item </b></h2>
-        <div className="additembg">
-          <Row>
-            <Col xs={6}>
-              {itemData.breakfast && itemData.breakfast.length > 0 ? (
-                <div className="menu-divison">
-                  <span className="item-name">--- Breakfast ---</span>
-                  <Row>
-                    <Col className="menu-column">
-                      {itemData.breakfast.map((val, index) => (
-                        <div className="menu-value">
-                          <span className="val-item">{val.name}</span>
-                          <span className="price">
-                            ----------- ${val.price}
-                          </span>
-                        </div>
-                      ))}
-                    </Col>
-                    <Col>
-                      <Image src={breakfast} className="image"></Image>
-                    </Col>
-                  </Row>
-                </div>
-              ) : (
-                ""
-              )}
-            </Col>
-            <Col xs={6}>
-              {itemData.bevergaes && itemData.bevergaes.length > 0 ? (
-                <div className="menu-divison">
-                  <span className="item-name">--- Beverages ---</span>
-                  <Row>
-                    <Col className="menu-column">
-                      {itemData.bevergaes.map((val, index) => (
-                        <div className="menu-value">
-                          <span className="val-item">{val.name}</span>
-
-                          <span className="price">
-                            ----------- ${val.price}
-                          </span>
-                        </div>
-                      ))}
-                    </Col>
-                    <Col>
-                      <Image src={bevergaes} className="image"></Image>
-                    </Col>
-                  </Row>
-                </div>
-              ) : (
-                ""
-              )}
-            </Col>
-            <Col xs={6}>
-              {itemData.dessert && itemData.dessert.length > 0 ? (
-                <div className="menu-divison">
-                  <span className="item-name">--- Dessert ---</span>
-                  <Row>
-                    <Col className="menu-column">
-                      {itemData.dessert.map((val, index) => (
-                        <div className="menu-value">
-                          <span className="val-item">{val.name}</span>
-                          <span className="price">
-                            ----------- ${val.price}
-                          </span>
-                        </div>
-                      ))}
-                    </Col>
-                    <Col>
-                      <Image src={dessert} className="image"></Image>
-                    </Col>
-                  </Row>
-                </div>
-              ) : (
-                ""
-              )}
-            </Col>
-            <Col xs={6}>
-              {itemData.rice && itemData.rice.length > 0 ? (
-                <div className="menu-divison">
-                  <span className="item-name">--- Rice ---</span>
-                  <Row>
-                    <Col className="menu-column">
-                      {itemData.rice.map((val, index) => (
-                        <div className="menu-value">
-                          <span className="val-item">{val.name}</span>
-                          <span className="price">
-                            ----------- ${val.price}
-                          </span>
-                        </div>
-                      ))}
-                    </Col>
-                    <Col>
-                      <Image src={rice} className="image"></Image>
-                    </Col>
-                  </Row>
-                </div>
-              ) : (
-                ""
-              )}
-            </Col>
-            <Col xs={6}>
-              {itemData.nonveg.chicken && itemData.nonveg.chicken.length > 0 ? (
-                <div className="menu-divison">
-                  <span className="item-name">--- Chicken ---</span>
-                  <Row>
-                    <Col className="menu-column">
-                      {itemData.nonveg.chicken.map((val, index) => (
-                        <div className="menu-value">
-                          <span className="val-item">{val.name}</span>
-                          <span className="price">
-                            ----------- ${val.price}
-                          </span>
-                        </div>
-                      ))}
-                    </Col>
-                    <Col>
-                      <Image src={chicken} className="image"></Image>
-                    </Col>
-                  </Row>
-                </div>
-              ) : (
-                ""
-              )}
-            </Col>
-          </Row>
+                Add more item
+              </Button>
+              <Button
+                className="additem_btnstyle"
+                type="submit"
+                onClick={mySubmitHandler}
+              >
+                Submit
+              </Button>
+            </Modal.Footer>
+          </Modal>
         </div>
-      </div>
+
+        <div>
+          <h2 className="heading">
+            <b>Menu Item </b>
+          </h2>
+          <div className="additembg">
+            <Row>
+              <Col xs={6}>
+                {itemData.breakfast && itemData.breakfast.length > 0 ? (
+                  <div className="menu-divison">
+                    <span className="item-name">--- Breakfast ---</span>
+                    <Row>
+                      <Col className="menu-column">
+                        {itemData.breakfast.map((val, index) => (
+                          <div className="menu-value">
+                            <span className="val-item">{val.name}</span>
+                            <span className="price">
+                              ----------- ${val.price}
+                            </span>
+                          </div>
+                        ))}
+                      </Col>
+                      <Col>
+                        <Image src={breakfast} className="image"></Image>
+                      </Col>
+                    </Row>
+                  </div>
+                ) : (
+                  ""
+                )}
+              </Col>
+              <Col xs={6}>
+                {itemData.bevergaes && itemData.bevergaes.length > 0 ? (
+                  <div className="menu-divison">
+                    <span className="item-name">--- Beverages ---</span>
+                    <Row>
+                      <Col className="menu-column">
+                        {itemData.bevergaes.map((val, index) => (
+                          <div className="menu-value">
+                            <span className="val-item">{val.name}</span>
+
+                            <span className="price">
+                              ----------- ${val.price}
+                            </span>
+                          </div>
+                        ))}
+                      </Col>
+                      <Col>
+                        <Image src={bevergaes} className="image"></Image>
+                      </Col>
+                    </Row>
+                  </div>
+                ) : (
+                  ""
+                )}
+              </Col>
+              <Col xs={6}>
+                {itemData.dessert && itemData.dessert.length > 0 ? (
+                  <div className="menu-divison">
+                    <span className="item-name">--- Dessert ---</span>
+                    <Row>
+                      <Col className="menu-column">
+                        {itemData.dessert.map((val, index) => (
+                          <div className="menu-value">
+                            <span className="val-item">{val.name}</span>
+                            <span className="price">
+                              ----------- ${val.price}
+                            </span>
+                          </div>
+                        ))}
+                      </Col>
+                      <Col>
+                        <Image src={dessert} className="image"></Image>
+                      </Col>
+                    </Row>
+                  </div>
+                ) : (
+                  ""
+                )}
+              </Col>
+              <Col xs={6}>
+                {itemData.rice && itemData.rice.length > 0 ? (
+                  <div className="menu-divison">
+                    <span className="item-name">--- Rice ---</span>
+                    <Row>
+                      <Col className="menu-column">
+                        {itemData.rice.map((val, index) => (
+                          <div className="menu-value">
+                            <span className="val-item">{val.name}</span>
+                            <span className="price">
+                              ----------- ${val.price}
+                            </span>
+                          </div>
+                        ))}
+                      </Col>
+                      <Col>
+                        <Image src={rice} className="image"></Image>
+                      </Col>
+                    </Row>
+                  </div>
+                ) : (
+                  ""
+                )}
+              </Col>
+              <Col xs={6}>
+                {itemData.nonveg.chicken &&
+                itemData.nonveg.chicken.length > 0 ? (
+                  <div className="menu-divison">
+                    <span className="item-name">--- Chicken ---</span>
+                    <Row>
+                      <Col className="menu-column">
+                        {itemData.nonveg.chicken.map((val, index) => (
+                          <div className="menu-value">
+                            <span className="val-item">{val.name}</span>
+                            <span className="price">
+                              ----------- ${val.price}
+                            </span>
+                          </div>
+                        ))}
+                      </Col>
+                      <Col>
+                        <Image src={chicken} className="image"></Image>
+                      </Col>
+                    </Row>
+                  </div>
+                ) : (
+                  ""
+                )}
+              </Col>
+            </Row>
+          </div>
+        </div>
       </div>
     </div>
   );
--- client/src/components/AdminPages/AdminWaiter.js
+++ client/src/components/AdminPages/AdminWaiter.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "../../css/admin.css";
+
 import "../../css/AdminStyles.css";
 import {
   Button,
@@ -22,82 +22,81 @@ const AdminWaiter = () => {
     setname(event.target.value);
   };
 
-  
   return (
     <div className="inner-container">
-    <div className="admin_parent_div_cards">
-      <Row className="admin_table_Design">
-        {waiter.map((user) => (
-          <Col xs={12} lg={4} md={6}>
-            {user.status === "Active" ? (
-              <CardDeck className="waiter_parent_card">
-                <Card className="waiterCard text-center mb-3 mt-3">
-                  <h4>
-                    {" "}
-                    <Card.Header className="waiterCardHeader">
-                      <b> {user.name} </b>
-                    </Card.Header>
-                  </h4>
-                  <Card.Body className="waiterCardBody">
-                    <b>Status:</b> {user.status}
-                    <br />
-                    <b>Total Order Taken:</b> {user.ordertaken}
-                    <br />
-                    <b> Table no: </b>
-                    {user.tableno}
-                    <br />
-                    <br />
-                    <Button
-                      className="waiterButton"
-                      onClick={handleShow}
-                      value={user.name}
-                    >
-                      Order Details
-                    </Button>
-                    <Modal
-                      show={show}
-                      onHide={handleClose}
-                      size="lg"
-                      aria-labelledby="contained-modal-title-vcenter"
-                      centered
-                    >
-                      <Modal.Header closeButton>Order Details</Modal.Header>
-                      <Modal.Body>
-                        {waiter.map((users) =>
-                          users.name === val
-                            ? users.orderdetails.map((number) => (
-                                <li>{number}</li>
-                              ))
-                            : ""
-                        )}
-                      </Modal.Body>
-                      <Modal.Footer>
-                        <Button variant="secondary" onClick={handleClose}>
-                          close
-                        </Button>
-                      </Modal.Footer>
-                    </Modal>
-                  </Card.Body>
-                </Card>
-              </CardDeck>
-            ) : (
-              <CardDeck>
-                <Card className="waiterAbsentCard text-center mb-3 mt-3">
-                  <h4>
-                    {" "}
-                    <Card.Header className="waiterCardHeader">
-                      <b> {user.name}</b>
-                    </Card.Header>
-                  </h4>
-                  <Card.Body className="waiterAbsentCardBody">
-                    <b>Status:</b> {user.status}
-                  </Card.Body>
-                </Card>
-              </CardDeck>
-            )}
-          </Col>
-        ))}
-      </Row>
+      <div className="admin_parent_div_cards">
+        <Row className="admin_table_Design">
+          {waiter.map((user) => (
+            <Col xs={12} lg={4} md={6}>
+              {user.status === "Active" ? (
+                <CardDeck className="waiter_parent_card">
+                  <Card className="waiterCard text-center mb-3 mt-3">
+                    <h4>
+                      {" "}
+                      <Card.Header className="waiterCardHeader">
+                        <b> {user.name} </b>
+                      </Card.Header>
+                    </h4>
+                    <Card.Body className="waiterCardBody">
+                      <b>Status:</b> {user.status}
+                      <br />
+                      <b>Total Order Taken:</b> {user.ordertaken}
+                      <br />
+                      <b> Table no: </b>
+                      {user.tableno}
+                      <br />
+                      <br />
+                      <Button
+                        className="waiterButton"
+                        onClick={handleShow}
+                        value={user.name}
+                      >
+                        Order Details
+                      </Button>
+                      <Modal
+                        show={show}
+                        onHide={handleClose}
+                        size="lg"
+                        aria-labelledby="contained-modal-title-vcenter"
+                        centered
+                      >
+                        <Modal.Header closeButton>Order Details</Modal.Header>
+                        <Modal.Body>
+                          {waiter.map((users) =>
+                            users.name === val
+                              ? users.orderdetails.map((number) => (
+                                  <li>{number}</li>
+                                ))
+                              : ""
+                          )}
+                        </Modal.Body>
+                        <Modal.Footer>
+                          <Button variant="secondary" onClick={handleClose}>
+                            close
+                          </Button>
+                        </Modal.Footer>
+                      </Modal>
+                    </Card.Body>
+                  </Card>
+                </CardDeck>
+              ) : (
+                <CardDeck>
+                  <Card className="waiterAbsentCard text-center mb-3 mt-3">
+                    <h4>
+                      {" "}
+                      <Card.Header className="waiterCardHeader">
+                        <b> {user.name}</b>
+                      </Card.Header>
+                    </h4>
+                    <Card.Body className="waiterAbsentCardBody">
+                      <b>Status:</b> {user.status}
+                    </Card.Body>
+                  </Card>
+                </CardDeck>
+              )}
+            </Col>
+          ))}
+        </Row>
       </div>
     </div>
   );
